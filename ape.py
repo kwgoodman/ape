ad575ebe306bcdb5e69d8989a21c4885d3dd07bb
Keith Goodman <kwgoodman@gmail.com>
2013-05-24 10:14:23 -0700

    ENH add exuberant-ctags; move license

--- ape.py
+++ ape.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2013, Berkeley Analytics, LLC.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import subprocess
 
 # ---------------------------------------------------------------------------
@@ -19,6 +44,7 @@ def get_pkg_list():
                 'meld.apt',
                 'grin.pip',
                 'flake8.pip',
+                'exuberant-ctags.apt',
 
                 # python
                 'python-dev.apt',
@@ -98,7 +124,7 @@ def pip(cmd, package, verbose=False):
     "pip install or remove"
     if cmd == 'install':
         pip_cmd = "pip install %s" % package
-    elif cmd == 'remove':    
+    elif cmd == 'remove':
         pip_cmd = "pip uninstall -y %s" % package
     else:
         raise ValueError("`cmd` must be 'install' or 'remove'")
